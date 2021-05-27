1427e9afd0916af397505db3ad0512bae59b8bac
Walery Strauch <walery@2iqresearch.com>
2021-05-27 15:11:32 +0200

    Fix formatting.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 try {
   console.log('Welcome to Get-XML-Version.')
-    
+
   var argv = require('minimist')(process.argv.slice(2));
   var xmlFile = (typeof argv.f !== 'undefined') ? argv.f : process.env.GITHUB_WORKSPACE+'/'+core.getInput('xml-file', { required: true });
   var xpathToSearch = (typeof argv.p !== 'undefined') ? argv.p : core.getInput('xpath', { required: true });
@@ -12,14 +12,14 @@ try {
   console.log(`XPath: ${xpathToSearch}`);
 
   var xpath = require('xpath'), dom = require('xmldom').DOMParser
- 
+
   fs.readFile(xmlFile, 'utf8', function read(err, data) {
     if (err) {
       core.setFailed(err.message);
     }
     else {
       console.log('File was read successfully. Proceeding to parse DOM.');
-      
+
       var doc = new dom().parseFromString(data);
       var nodes = xpath.select(xpathToSearch, doc);
 
